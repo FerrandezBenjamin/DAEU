7f7dbc4ced154166bb758f2bc594a756768dc3cf
FerrandezBenjamin <58741067+FerrandezBenjamin@users.noreply.github.com>
2025-12-17 19:24:52 +0100

    update cours php1

--- DAEU_PHP/Cours_1_Decouverte/index.php
+++ DAEU_PHP/Cours_1_Decouverte/index.php
@@ -1,14 +1,13 @@
 <?php 
 
-$name   = "Benjamin";
-$age    = 30;
-$adress = "28 Av. du Lac d'Annecy, 73370 Le Bourget-du-Lac";
+// A faire :
+// Je veux une fonction qui me permet de me retourner mon prénom, mon nom
+// ainsi que mon adresse.
+// Lorsque j'execute php index.php
+// Alors Quelque chose comme :
+// Je m'appelle Benjamin. J'ai 30 ans et mon adresse est : 28 Av. du Lac d'Annecy, 73370 
+// Le Bourget-du-Lac 
+// apparait
 
 
-function exprimeToi($nomDeLaVariableDuNom, $nomDeLaVariableDeLage, $nomDeLaVariableDeLadress)
-{
-    echo "Je m'appelle " . $nomDeLaVariableDuNom . ". J'ai " . $nomDeLaVariableDeLage . " ans et mon adresse est : " .$nomDeLaVariableDeLadress;
-}
-
-exprimeToi($name, $age, $adress);
 ?>
