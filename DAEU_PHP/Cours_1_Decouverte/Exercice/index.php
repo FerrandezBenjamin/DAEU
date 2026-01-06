d5db1d1937b09c9f5f16c3ad62a9ec1e0e7a12bf
Pc-Ben-Win11 <=ferrandez.73000@yahoo.fr>
2026-01-06 19:24:39 +0100

    cours 1 php decouverte

---
+++ DAEU_PHP/Cours_1_Decouverte/Exercice/index.php
@@ -0,0 +1,51 @@
+<?php 
+
+// ------------------------ 1) :
+// Je veux une fonction qui me permet de me retourner mon prénom, mon nom
+// ainsi que mon adresse.
+// Lorsque j'execute php index.php
+// Alors Quelque chose comme :
+// Je m'appelle Benjamin. J'ai 30 ans et mon adresse est : 28 Av. du Lac d'Annecy, 73370 
+// Le Bourget-du-Lac 
+// apparait
+
+
+// ------------------------ 2) :
+// Je veux une fonction qui prend en paramètre 2 nombres
+// et qui me retourne la phrase suivante :
+//
+// "La somme de X et Y est égale à Z"
+//
+// Lorsque j'exécute php index.php
+// Alors quelque chose comme :
+// La somme de 5 et 7 est égale à 12
+// apparaît
+
+
+// ------------------------ 3) :
+// Je veux une fonction qui prend en paramètre 2 nombres
+// et qui affiche quel est le plus grand des deux.
+// Si les deux nombres sont égaux,
+// Alors un message doit indiquer qu'ils sont identiques.
+//
+// Lorsque j'exécute php index.php
+// Alors quelque chose comme :
+// 10 est plus grand que 7
+// apparaît
+
+
+// ------------------------ 4) :
+// Je veux une fonction qui prend en paramètre un âge.
+// Si l'âge est supérieur ou égal à 18,
+// Alors le message suivant doit s'afficher :
+// "La personne est majeure."
+// Sinon le message suivant doit s'afficher :
+// "La personne est mineure."
+//
+// Lorsque j'exécute php index.php
+// Alors quelque chose comme :
+// La personne est majeure.
+// apparait
+
+
+?>
