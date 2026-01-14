101499816e6e9938b64eb00398fab0b5f6665f6c
FerrandezBenjamin <58741067+FerrandezBenjamin@users.noreply.github.com>
2026-01-14 18:31:05 +0100

    add exo

--- DAEU_PHP/Cours_1_Decouverte/index.php
+++ DAEU_PHP/Cours_1_Decouverte/index.php
@@ -10,4 +10,27 @@
 // apparait
 
 
+
+
+// Exercice 4 :
+// Je veux une fonction qui vérifie si un mot de passe est valide.
+// Le mot de passe doit respecter 2 règles :
+//
+// 1 — Le mot de passe doit faire au moins 8 caractères
+// 2 — Le mot de passe doit contenir au moins un chiffre (entre 0 et 9)
+//
+// Lorsque j'exécute : php index.php
+// Alors des messages comme :
+//
+// Erreur : le mot de passe doit faire au moins 8 caractères.
+// OU
+// Erreur : le mot de passe doit contenir au moins un chiffre.
+// OU
+// Mot de passe valide
+//
+// doivent apparaître à l'écran.
+//
+//
+// (Tu peux ensuite tester ta fonction avec plusieurs mots de passe)
+
 ?>
